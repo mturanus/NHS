5fc0808485185c196e17bda3dc5d48bd34f61eec
davidcitak <edavidcitak@gmail.com>
2023-02-15 15:39:52 -0600

    This changes made with NA-2 branch

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,5 +4,6 @@ public class Test {
         System.out.println("Test");
         System.out.println();
         System.out.println("This is updates from the with new branch");
+        System.out.println("This is updates from the NA-2 branch");
     }
 }
